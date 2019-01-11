428f418b0bc87e16c0b3ca9fdd4e1c9620531dd4
rashidfarook1 <rashid.farook@verizonwireless.com>
2019-01-11 11:28:47 -0500

    Updates

---
+++ precache-manifest.f247250aa50d4cfae0cbc96ca52b9c5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f98747aee436af44b33c",
+    "url": "/my-appt/static/js/runtime~main.f98747ae.js"
+  },
+  {
+    "revision": "dd2db63ed0da1fb7f9cb",
+    "url": "/my-appt/static/js/main.dd2db63e.chunk.js"
+  },
+  {
+    "revision": "40cfcd44bb881ac829c8",
+    "url": "/my-appt/static/js/1.40cfcd44.chunk.js"
+  },
+  {
+    "revision": "dd2db63ed0da1fb7f9cb",
+    "url": "/my-appt/static/css/main.205fddf9.chunk.css"
+  },
+  {
+    "revision": "d7c4ea61abf2e61ddfe71e597d613f8d",
+    "url": "/my-appt/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-appt/precache-manifest.a57339b557b189a147b53251b5c363e0.js"
+  "/my-appt/precache-manifest.f247250aa50d4cfae0cbc96ca52b9c5b.js"
 );
 
 workbox.clientsClaim();
